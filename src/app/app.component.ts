0109b56dd833b777afaa0127cdc546d478a773f6
murugan-kannan <machaendran9221@gmail.com>
2021-08-03 15:21:42 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,21 @@ import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   show = false;
 
   @ViewChild('password') password: ElementRef;
 
-  toggle(){
-   this.show = !this.show;
-   if(this.show){
-   this.password.nativeElement.type = "text";
-   }else{
-    this.password.nativeElement.type = "password";
-   }
+  toggle() {
+    this.show = !this.show;
+    if (this.show) {
+      this.password.nativeElement.type = 'text';
+    } else {
+      this.password.nativeElement.type = 'password';
+    }
   }
-
 }
